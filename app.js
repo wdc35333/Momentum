1af966dfc6ec881c59d8167c5609df46547e62e1
wdc35333 <wdc35333@gmail.com>
2021-09-28 17:04:05 +0900

    모멘툼 로그인 업데이트

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
 
 const link = document.querySelector("a");
 
+
+const HIDDEN_CLASSNAME = "hidden";
+
+
+
 function onLoginSubmit(event) {
-    const username = loginInput.value;
     event.preventDefault();
-    console.log(username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
