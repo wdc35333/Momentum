66bb7aa26ddedaeb4dace0d78964efebba46d6b6
wdc35333 <wdc35333@gmail.com>
2021-09-28 12:06:10 +0900

    Momentum Login 중간저장

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-function handleWindowResize() {
-    let intViewportWidth = window.innerWidth;
+const link = document.querySelector("a");
 
-    if (intViewportWidth > 1600) {
-        document.body.classList.remove("middle");
-        document.body.classList.add("big");
-        
-    } else if (intViewportWidth < 1600&&intViewportWidth > 1200) {
-        document.body.classList.remove("big");
-        document.body.classList.remove("small");
-        document.body.classList.add("middle");
-
-    } else {
-        document.body.classList.remove("middle");
-        document.body.classList.add("small");
-
-    }
-
+function onLoginSubmit(event) {
+    const username = loginInput.value;
+    event.preventDefault();
+    console.log(username);
 }
-window.addEventListener("resize", handleWindowResize);
+
+
+function handleLinkClick(event){
+    event.preventDefault();
+    console.dir(event);
+}
+
+
+loginForm.addEventListener("submit",onLoginSubmit);
+link.addEventListener("click",handleLinkClick);
