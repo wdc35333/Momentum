a8935d01c0f4aefce50f299988da673928e2d998
wdc35333 <wdc35333@gmail.com>
2021-09-28 17:19:28 +0900

    적은 수정

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
-// const loginForm = document.querySelector("#login-form");
-// const loginInput = document.querySelector("#login-form input");
-// const greeting = document.querySelector("#greeting");
-const maxForm = document.querySelector("#max-num");
-const maxInput = document.querySelector('#max-num input');
-const selectForm = document.querySelector("#select-num");
-const selectInput = document.querySelector('#select-num input');
+const maxForm = document.querySelector("#maxNum");
+const maxInput = document.querySelector('#maxNum input');
+const selectForm = document.querySelector("#selectNum");
+const selectInput = document.querySelector('#selectNum input');
 const choose = document.querySelector("#choose");
 const result = document.querySelector("#result");
 
 
-const HIDDEN_CLASSNAME = "hidden";
-// const link = document.querySelector("a");
 
-// function onLoginSubmit(event) {
-//     event.preventDefault();
-//     loginForm.classList.add(HIDDEN_CLASSNAME);
-//     const username = loginInput.value;
-//     greeting.innerText = `Hello ${username}`;
-//     greeting.classList.remove(HIDDEN_CLASSNAME);
-// }
-
-
-function onMaxNumSubmit(event) {
+function onNumGameSubmit(event) {
     event.preventDefault();
     const maxNum = maxInput.value;
     const selectNum = selectInput.value;
-    choose.innerText = `You chose: ${selectNum}, the machine chose: ${getRandomAribitrary(maxNum)}.`
-    if (getRandomAribitrary(maxNum) === selectNum) {
+    const randomValue = getRandomAribitrary(maxNum);
+    choose.innerText = `You chose: ${selectNum}, the machine chose: ${randomValue}.`
+    if (randomValue === selectNum) {
         result.innerText = "You won!";
     } else {
         result.innerText = "You lose!";
@@ -34,10 +21,7 @@ function onMaxNumSubmit(event) {
     
 }
 
-// function handleLinkClick(event) {
-//     event.preventDefault();
-//     console.dir(event);
-// }
+
 
 function getRandomAribitrary(max) {
     max.preventDefault();
@@ -45,6 +29,4 @@ function getRandomAribitrary(max) {
 }
 
 
-maxForm.addEventListener("submit", onMaxNumSubmit);
-// loginForm.addEventListener("submit", onLoginSubmit);
-// link.addEventListener("click", handleLinkClick);
+selectForm.addEventListener("submit", onNumGameSubmit);
