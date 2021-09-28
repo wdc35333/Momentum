98bf6f6f0babb202faa599998b1b37ba8695492c
wdc35333 <wdc35333@gmail.com>
2021-09-28 19:17:15 +0900

    resize eventListener backup

--- app.js
+++ app.js
@@ -1,39 +1,22 @@
-const maxForm = document.querySelector("#maxNum");
-const maxInput = document.querySelector('#maxNum input');
-const selectForm = document.querySelector("#selectNum");
-const selectInput = document.querySelector('#selectNum input');
-const choose = document.querySelector("#choose");
-const result = document.querySelector("#result");
+const h1 = document.querySelector("div.hello:first-child h1");
 
+function handleWindowResize() {
+    let intViewportWidth = window.innerWidth;
 
+    if (intViewportWidth > 1600) {
+        document.body.classList.remove("middle");
+        document.body.classList.add("big");
+        
+    } else if (intViewportWidth < 1600&&intViewportWidth > 1200) {
+        document.body.classList.remove("big");
+        document.body.classList.remove("small");
+        document.body.classList.add("middle");
 
-const HIDDEN_CLASSNAME = "hidden";
-
-function onNumGameSubmit(event) {
-    event.preventDefault();
-    const maxNum = maxInput.value;
-    const selectNum = selectInput.value;
-    const randomValue = getRandomAribitrary(maxNum);
-    console.log(randomValue);
-    choose.innerText = `You chose: ${selectNum}, the machine chose: ${randomValue}.`
-    if (randomValue == selectNum) {
-        choose.classList.remove(HIDDEN_CLASSNAME);
-        result.classList.remove(HIDDEN_CLASSNAME);
-        result.innerText = "You won!";
     } else {
-        choose.classList.remove(HIDDEN_CLASSNAME);
-        result.classList.remove(HIDDEN_CLASSNAME);
-        result.innerText = "You lose!";
+        document.body.classList.remove("middle");
+        document.body.classList.add("small");
+
     }
-    
+
 }
-
-
-
-function getRandomAribitrary(max) {
-    
-    return Math.round(Math.random() * max);
-}
-
-
-selectForm.addEventListener("submit", onNumGameSubmit);
+window.addEventListener("resize", handleWindowResize);
