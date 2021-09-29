2b8b96793c9518db10e5f53758591cb2136a8d77
wdc35333 <wdc35333@gmail.com>
2021-09-29 15:42:49 +0900

    로그인 저장 구현

--- app.js
+++ app.js
@@ -3,27 +3,34 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 
-const link = document.querySelector("a");
+
 
 
 const HIDDEN_CLASSNAME = "hidden";
 
-
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    greeting.innerText = `Hello ${username}`;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    
 }
 
+loginForm.addEventListener("submit", onLoginSubmit);
 
-function handleLinkClick(event){
-    event.preventDefault();
-    console.dir(event);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
 }
-
-
-loginForm.addEventListener("submit",onLoginSubmit);
-link.addEventListener("click",handleLinkClick);
