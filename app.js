ed4a35a23927f210bc2d801e598b615e29982cf0
wdc35333 <wdc35333@gmail.com>
2021-09-29 15:55:47 +0900

    로그인 약간 수정

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 
-
-
-
 const HIDDEN_CLASSNAME = "hidden";
-
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
@@ -15,24 +11,22 @@ function onLoginSubmit(event) {
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+    paintGreetings();
 }
 
-function paintGreetings(username) {
+function paintGreetings() {
+    const username = localStorage.getItem(USERNAME_KEY);
     greeting.classList.remove(HIDDEN_CLASSNAME);
     greeting.innerText = `Hello ${username}`;
-    
-
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-
 if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintGreetings(savedUsername);
+    paintGreetings();
 }
