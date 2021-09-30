134f9a0d36d9a8e7baa7191b7720c314821d245f
wdc35333 <wdc35333@gmail.com>
2021-09-30 13:17:50 +0900

    배경화면 색 변경 과제 초기설정

--- js/background.js
+++ js/background.js
@@ -1,8 +1,24 @@
-const images = [
-    "0.jpeg",
-    "1.jpeg",
-    "2.jpeg"
-]
+const colors = [
+    "#ef5777",
+    "#575fcf",
+    "#4bcffa",
+    "#34e7e4",
+    "#0be881",
+    "#f53b57",
+    "#3c40c6",
+    "#0fbcf9",
+    "#00d8d6",
+    "#05c46b",
+    "#ffc048",
+    "#ffdd59",
+    "#ff5e57",
+    "#d2dae2",
+    "#485460",
+    "#ffa801",
+    "#ffd32a",
+    "#ff3f34"
+];
+
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
